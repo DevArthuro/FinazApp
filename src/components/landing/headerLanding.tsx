2b4e066a172e7192e49fdabb447ddc0f580025b6
DevArthuro <caorrego38@soy.sena.edu.co>
2024-05-28 22:53:53 -0500

    feat: add administration with clerk

--- src/components/landing/headerLanding.tsx
+++ src/components/landing/headerLanding.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
-    <div className="p-5 flex justify-between items-center border shadow-md">
-      <Image src={"logo.svg"} alt="logo finazpp" width={160} height={100} />
-      <Button>Ver más</Button>
+    <div className="p-5 flex justify-between items-center bg-[#FF6B35]">
+      <Image src={"/logo.svg"} alt="logo finazpp" width={160} height={100} />
+      <SignedOut>
+        <SignInButton>
+          <Button>Panel Principal</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 };
