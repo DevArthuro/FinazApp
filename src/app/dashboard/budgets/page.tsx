73d2c1604909a69618513d5d083f025ecef6d100
Carlos Orrego <carlosorrego5151@gmail.com>
2024-10-13 23:33:47 -0500

    feat: clean project

--- src/app/dashboard/budgets/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Budgets = () => {
-  return <h1>Budgets</h1>;
-};
-
-export default Budgets;
--- src/app/dashboard/upgrades/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Upgrades = () => {
-  return <div>Upgrades</div>;
-};
-
-export default Upgrades;
